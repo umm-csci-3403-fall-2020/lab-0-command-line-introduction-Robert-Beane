c94aaa14865cf2c3fef43ce9e09b7b35e1a78d75
Robert-Beane <beane031@morris.umn.edu>
2020-08-25 11:18:32 -0500

    Fix extract_and_compile.sh to properly disply the proper output and prime number.

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -4,8 +4,7 @@
 
 # Parameters
 number=$1
-dir=$2
 
-tar xvfz NthPrime.tgz --directory /$dir
-gcc -o NthPrime $dir/NthPrime/main.c $dir/NthPrime/nth_prime.c
-./build $dir/NthPrime
+tar xfz NthPrime.tgz 
+gcc NthPrime/main.c NthPrime/nth_prime.c -o NthPrime/NthPrime
+./NthPrime/NthPrime $number
