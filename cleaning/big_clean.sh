4f52444c712f22ef3cba5b90e1890053ab93d9eb
Robert-Beane <beane031@morris.umn.edu>
2020-08-26 20:25:18 -0500

    Fix big_clean.sh to properly clean directorys and re-pack them.

---
+++ cleaning/big_clean.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Robert Beane
+
+# Parameters
+comp=$1 # takes inputed file
+name=$(basename "$comp") # gets full name of file (.tgz)
+noext=$(basename "${comp%.*}") # gets directory name with no extension
+here=$(pwd) # current working directory
+
+# temp directory generator
+tmp_dir=$(mktemp -d -t clean-XXX)
+
+# Extraction
+tar xzf "$name" -C "$tmp_dir"
+
+# Look for "DELETE ME!" and remove all files containing it
+grep "DELETE ME!" -rlZ "$tmp_dir" | xargs -0 rm
+
+# Compress the new, cleaned directory
+cd "$tmp_dir"
+tar czf cleaned_"$name" "$noext"
+
+# move the tgz to the correct directory
+mv "$tmp_dir"/cleaned_"$name" "$here"
